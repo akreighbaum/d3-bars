f88870f7f9b5164a5ce0c4773269b735317c8571
akreighbaum <andkreigh@gmail.com>
2015-02-15 10:59:42 -0600

    Graphic of total hits by season
    Bar chart w/ comments added to javascript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,70 @@
-$(document).ready(function() {
-    console.log("Hello world.")
+//1. Sets the margins of conventions of all four sides of the graphic object 
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = $(".chart").width() - margin.left - margin.right,
+    height = $(".chart").height() - margin.top - margin.bottom;
+
+//2. Sets the width and height of the inner dimensions of the graphic 
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+//3. Sets conventions of the horizontal and vertical axes in the chart 
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10)
+    .tickFormat(function(d) {
+      return d;
+    });
+
+//4. 'Selects' the standard vector graphic element that draws the bars in the chart 
+var svg = d3.select(".chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("kreighbaum_baseball_card.json", function(error, data) {
+
+	console.log(data);
+
+//5. Scales values of data and fits them into space available
+  x.domain(data.stats.map(function(d) { return d.year; }));
+  y.domain([0, d3.max(data.stats, function(d) { return d.H; })]);
+
+//6. The 'g' element groups elements together in the standard vector graphic. After grouping, the shapes can be transformed as if they were a single shape.
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Hits");
+
+//7. Calls the data used to create the rectangles or bars in the graphic
+  svg.selectAll(".bar")
+      .data(data.stats)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.year); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.H); })
+      .attr("height", function(d) { return height - y(d.H); });
+
 });
-
-
-
-
 
 
